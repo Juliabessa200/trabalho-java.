90e86095476e45cf97b9a7d258ac2aff0209cd73
Juliabessa200 <102189088+Juliabessa200@users.noreply.github.com>
2022-04-12 19:05:52 -0300

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,46 @@
+public static void main ( String [] args ) {
+		Sistema. para . println("Questão_1");
+		
+		duplo pi = 3,14159 ;
+		 proporção dupla = 21 ;
+		double areaCirculo = pi * ( raio * raio );
+		
+		Sistema. para . println("Área do Círculo = " + areaCírculo);
+		Sistema. para . println();
+		
+		
+		Sistema. para . println("Questão_2");
+		
+		duplo A = 10 ;
+		duplo B = 15 ;
+		duplo C = 20 ;
+		double areadoTriângulo = ( A * C )/ 2 ;
+		double areadoCirculo2 = pi * ( C * C );
+		double areadoTrapezioPT1 = ( A + B ) * C ;
+		double areadoTrapezioPT2 = areadoTrapezioPT1 / 2 ;
+		duplo areadoQuadrado = B*B;
+		double areadoRetangulo = A * B ;
+		
+		Sistema. para . println("Área do Triângulo = " + areadoTriângulo);
+		Sistema. para . println("Área do Círculo = " + areadoCírculo2);
+		Sistema. para . println("Área do Trapézio = " + areadoTrapézioPT2);
+		Sistema. para . println("Área do Quadrado=" + areadoQuadrado);
+		Sistema. para . println("Área do Retângulo = " + areadoRetângulo);
+		Sistema. para . println();
+		
+		
+		Sistema. para . println("Questão_3");
+		
+		duplo a = 3 ;
+		duplo b = - 15 ;
+		duplo c = 12 ;
+		duplo delta = matemática . pow(b,2,0) -4*a*c;
+		double x1 = (- b + Math . sqrt ( delta )) / ( 2,0 * a );
+		double x2 = (- b - Math. sqrt ( delta )) / ( 2,0 * a );
+		
+		Sistema. para . println("x1 = "+x1);
+		Sistema. para . println("x2 = "+x2);
+		
+    }
+	
+}
